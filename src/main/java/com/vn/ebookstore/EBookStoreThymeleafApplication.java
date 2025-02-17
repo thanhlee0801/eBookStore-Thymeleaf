eed1d98beb4d10d92486c986d9527dbf7ebb1a7c
Phạm Quốc Tuấn <qtuan.ph@gmail.com>
2025-02-17 21:43:47 +0700

    Initial commit

---
+++ src/main/java/com/vn/ebookstore/EBookStoreThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.vn.ebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookStoreThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookStoreThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vn/ebookstore/EBookStoreThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.ebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookStoreThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

028b1b0f17ff3d673654a2c94bd63897d207402a
Phạm Quốc Tuấn <qtuan.ph@gmail.com>
2025-02-22 01:08:17 +0700

    Chuan bi hien thi anh len db

--- src/main/java/com/vn/ebookstore/controller/NavbarController.java
+++ src/main/java/com/vn/ebookstore/controller/NavbarController.java
@@ -1,14 +1,24 @@
 package com.vn.ebookstore.controller;
 
+import com.vn.ebookstore.model.Category;
+import com.vn.ebookstore.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class NavbarController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/")
     public String home(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
         return "index"; // Tương ứng với src/main/resources/templates/index.html
     }
 

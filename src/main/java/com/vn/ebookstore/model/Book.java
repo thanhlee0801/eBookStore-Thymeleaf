9dfa9d8cd6d048e52603511b0dd52c73e77bfa90
Phạm Quốc Tuấn <qtuan.ph@gmail.com>
2025-02-22 11:48:45 +0700

    cmt

--- src/main/java/com/vn/ebookstore/model/Book.java
+++ src/main/java/com/vn/ebookstore/model/Book.java
@@ -2,7 +2,6 @@ package com.vn.ebookstore.model;
 
 import jakarta.persistence.*;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Table(name = "books")
@@ -21,20 +20,6 @@ public class Book {
     @ManyToOne
     @JoinColumn(name = "sub_category_id", insertable = false, updatable = false)
     private SubCategory subCategory;
-
-    @OneToMany(mappedBy = "book")
-    private List<BookDetail> bookDetails;
-
-    @OneToMany(mappedBy = "book")
-    private List<CartItem> cartItems;
-
-    @OneToMany(mappedBy = "book")
-    private List<OrderItem> orderItems;
-
-    @OneToMany(mappedBy = "book")
-    private List<Wishlist> wishlists;
-
-    // Getters and Setters
 
     public int getId() {
         return id;
@@ -98,37 +83,5 @@ public class Book {
 
     public void setSubCategory(SubCategory subCategory) {
         this.subCategory = subCategory;
-    }
-
-    public List<BookDetail> getBookDetails() {
-        return bookDetails;
-    }
-
-    public void setBookDetails(List<BookDetail> bookDetails) {
-        this.bookDetails = bookDetails;
-    }
-
-    public List<CartItem> getCartItems() {
-        return cartItems;
-    }
-
-    public void setCartItems(List<CartItem> cartItems) {
-        this.cartItems = cartItems;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    public List<Wishlist> getWishlists() {
-        return wishlists;
-    }
-
-    public void setWishlists(List<Wishlist> wishlists) {
-        this.wishlists = wishlists;
     }
 }

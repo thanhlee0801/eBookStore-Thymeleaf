b828eee0b65ed9693689a361b87e5f5c9ce39d22
Trương Tuấn Kiệt <contacttuankiet.dev@gmail.com>
2025-07-03 08:59:20 +0700

    sửa AdminController thành DashboardController

--- src/main/java/com/vn/ebookstore/controller/admin/DashboardController.java
+++ src/main/java/com/vn/ebookstore/controller/admin/DashboardController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
-public class AdminController {
+public class DashboardController {
 
     @Autowired
     private OrderDetailService orderDetailService;
